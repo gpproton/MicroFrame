30e9dfb04f7dc0fdbbcb756f53086bbb6024f08d
Godwin peter .O <me@godwin.dev>
2020-05-18 13:17:36 +0100

    Update ErrorView.php

--- Views/ErrorView.php
+++ Views/ErrorView.php
@@ -9,11 +9,11 @@ final class ErrorView extends BaseView{
 ?>
 
     <div>
-        <div class="dl_horizontal_center">
-            <span class="dl_header_text">Error encountered</span>
+        <div class="tlr_horizontal_center">
+            <span class="tlr_header_text">Error encountered</span>
 
-            <div class="dl_status_icon" style="margin-top: 35px;">
-                <span class="material-icons dl_status_icon_error">error</span>
+            <div class="tlr_status_icon" style="margin-top: 35px;">
+                <span class="material-icons tlr_status_icon_error">error</span>
             </div>
         </div>
     </div>
