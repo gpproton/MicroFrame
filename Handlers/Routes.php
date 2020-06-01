86fea916127af6930f8b2ebca92d02b5eb1cc0f6
Godwin peter .O <me@godwin.dev>
2020-06-01 12:35:05 +0100

    Completed test

--- Handlers/Routes.php
+++ Handlers/Routes.php
@@ -69,21 +69,6 @@ final class Routes {
 
     private static function StartController()
     {
-        $testModel = new Model('Models_InvoiceList');
-        $testModel->queryParams = array(':rownums' => 5);
-
-        // $testModel->queryString = "SELECT * FROM
-        // (SELECT oft.FT_TXN_NO, oft.FT_NEW_VEHICLE_NUMBER
-        // FROM OT_FLEET_TIME oft)
-        // WHERE ROWNUM <= 5";
-
-        // $testModel->queryReturn = true;
-
-        // while ($row = $testModel->queryPrepare->fetch(PDO::FETCH_ASSOC))
-        // {
-        //     echo var_dump($row) . '<br>';
-        // }
-
         Utils::viewLoader(self::$RouteMode);
         StartView::Render();
     }
