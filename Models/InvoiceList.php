428111448386d29ee3c894d9b349a4b52e1b965b
Godwin peter .O <me@godwin.dev>
2020-06-01 18:36:57 +0100

    Update InvoiceList.php

--- Models/InvoiceList.php
+++ Models/InvoiceList.php
@@ -2,10 +2,33 @@
 
 function getQueryString()
 {
-    return "SELECT * FROM
-    (SELECT oft.FT_TXN_NO, oft.FT_NEW_VEHICLE_NUMBER
-    FROM OT_FLEET_TIME oft)
-    WHERE ROWNUM <= :rownums";
+    return "SELECT
+	oft.FT_NEW_VEHICLE_NUMBER,
+	zud.ZUD_CR_DT,
+	otihf.TINVH_TXN_CODE,
+	otihf.TINVH_NO,
+	otihf.TINVH_DT,
+	otirf.TINVR_REF_TXN_CODE,
+	otirf.TINVR_REF_NO,
+	othf.TOH_FLEX_06 FT_TXN_NO,
+	oft.FT_DELIVER_NO,
+	zud.ZUD_DOC_ADDR
+FROM
+	OT_TRIP_INVOICE_HEAD_FIN otihf,
+	OT_TRIP_INVOICE_REF_FIN otirf,
+	OT_TO_HEAD_FIN othf,
+	OT_FLEET_TIME oft
+LEFT JOIN ZWB_UPLOAD_DETAIL zud ON oft.FT_DELIVER_NO = zud.ZUD_DELIVER_NO
+WHERE
+	otirf.TINVR_TINVH_SYS_ID = otihf.TINVH_SYS_ID
+	AND otihf.TINVH_NO = :TINVH_NO
+	AND otihf.TINVH_COMP_CODE = 'MCPL006'
+	AND othf.TOH_COMP_CODE = 'MCPL006'
+	AND otihf.TINVH_TXN_CODE = :TINVH_TXN_CODE
+	AND otirf.TINVR_REF_TXN_CODE = othf.TOH_TXN_CODE
+	AND otirf.TINVR_REF_NO = othf.TOH_NO
+    AND TO_NUMBER(NVL(othf.TOH_FLEX_06, '0')) = oft.FT_TXN_NO
+    AND oft.FT_DELIVER_NO IS NOT NULL";
 }
 
 function getQueryReturn()
