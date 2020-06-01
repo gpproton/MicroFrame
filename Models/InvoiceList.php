010166daab3bd70398557125eae6bd4a5f4a1537
Godwin peter .O <me@godwin.dev>
2020-06-01 12:36:02 +0100

    Invoice test query

--- Models/InvoiceList.php
+++ Models/InvoiceList.php
@@ -1,8 +1,14 @@
 <?php
 
-$queryString = "SELECT * FROM
-(SELECT oft.FT_TXN_NO, oft.FT_NEW_VEHICLE_NUMBER
-FROM OT_FLEET_TIME oft)
-WHERE ROWNUM <= :rownums";
+function getQueryString()
+{
+    return "SELECT * FROM
+    (SELECT oft.FT_TXN_NO, oft.FT_NEW_VEHICLE_NUMBER
+    FROM OT_FLEET_TIME oft)
+    WHERE ROWNUM <= :rownums";
+}
 
-$queryReturn = true;
+function getQueryReturn()
+{
+    return true;
+}
