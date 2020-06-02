b6787750dbe2cdcb97332b4d9f06cf62d86d1fa8
Godwin peter .O <me@godwin.dev>
2020-06-02 15:22:26 +0100

    Update SearchView.php

--- Views/SearchView.php
+++ Views/SearchView.php
@@ -23,14 +23,16 @@ final class SearchView extends BaseView {
             <div>
                 <span class="tlr_horizontal_center" style="font-weight: 200; font-size: 5em; color: #942621;">Invoice Search</span>
             </div>
-            <div class="mdl-textfield mdl-js-textfield tlr_horizontal_center" style="margin-top: 120px;">
-                <input class="mdl-textfield__input tlr_input" type="text" id="tlr_search" pattern="-?[0-9]*(\.[0-9]+)?" style="border-color: #942621;">
-                <label class="mdl-textfield__label" for="tlr_search">Invoice No</label>
-                <span class="mdl-textfield__error">Input is not a number!</span>
-                <button class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored tlr_button" name="tlr_submit_login">
-                    <i class="material-icons">search</i>
-                </button>
-            </div>
+            <form action="" method="POST" class="tlr_form">
+                <div class="mdl-textfield mdl-js-textfield tlr_horizontal_center" style="margin-top: 120px;">
+                    <input class="mdl-textfield__input tlr_input" type="text" id="tlr_search" name="tlr_search_invoice" pattern="-?[0-9]*(\.[0-9]+)?" style="border-color: #942621;">
+                    <label class="mdl-textfield__label" for="tlr_search">Invoice No</label>
+                    <span class="mdl-textfield__error">Input is not a number!</span>
+                    <button class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored tlr_button" name="tlr_submit_search">
+                        <i class="material-icons">search</i>
+                    </button>
+                </div>
+            </form>
          </div>
     </div>
 
