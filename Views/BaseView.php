24acaac8c98956e7b75f9b26bf4f7bb8176138b7
Godwin peter .O <me@godwin.dev>
2020-06-16 13:30:26 +0100

    Assets variable

--- Views/BaseView.php
+++ Views/BaseView.php
@@ -18,6 +18,7 @@ class BaseView {
         
         $baseUrl = Routes::PageActualUrl();
         $iconImage = "<a href='" . $baseUrl . "'><img src='" . $baseUrl . "Assets/Icons/apple-icon.png' style='height: 32px;' alt='logo' /></a>";
+        $iconsBasePath = $baseUrl . 'Assets/Icons';
         $logOutLink = "";
         $faqsLink = "<a class='mdl-navigation__link' href='" . Routes::PageActualUrl(Config::ALLOWED_QUERY_STRINGS[0]) . "'>FAQ's</a>";
         if(Auth::Verify()) $logOutLink = "<a class='mdl-navigation__link' href='" . Routes::PageActualUrl(Config::ALLOWED_QUERY_STRINGS[5]) . "'>Sign Out</a>";
@@ -36,22 +37,22 @@ class BaseView {
                 <link rel="stylesheet" href="./Libs/style/material.min.css">
                 <link rel="stylesheet" href="./Libs/style/main.css">
                 <!-- Load Icons / Logos -->
-                <link rel="apple-touch-icon" sizes="57x57" href="/Assets/Icons/apple-icon-57x57.png">
-                <link rel="apple-touch-icon" sizes="60x60" href="/Assets/Icons/apple-icon-60x60.png">
-                <link rel="apple-touch-icon" sizes="72x72" href="/Assets/Icons/apple-icon-72x72.png">
-                <link rel="apple-touch-icon" sizes="76x76" href="/Assets/Icons/apple-icon-76x76.png">
-                <link rel="apple-touch-icon" sizes="114x114" href="/Assets/Icons/apple-icon-114x114.png">
-                <link rel="apple-touch-icon" sizes="120x120" href="/Assets/Icons/apple-icon-120x120.png">
-                <link rel="apple-touch-icon" sizes="144x144" href="/Assets/Icons/apple-icon-144x144.png">
-                <link rel="apple-touch-icon" sizes="152x152" href="/Assets/Icons/apple-icon-152x152.png">
-                <link rel="apple-touch-icon" sizes="180x180" href="/Assets/Icons/apple-icon-180x180.png">
-                <link rel="icon" type="image/png" sizes="192x192"  href="/Assets/Icons/android-icon-192x192.png">
-                <link rel="icon" type="image/png" sizes="32x32" href="/Assets/Icons/favicon-32x32.png">
-                <link rel="icon" type="image/png" sizes="96x96" href="/Assets/Icons/favicon-96x96.png">
-                <link rel="icon" type="image/png" sizes="16x16" href="/Assets/Icons/favicon-16x16.png">
-                <link rel="manifest" href="/Assets/Icons/manifest.json">
+                <link rel="apple-touch-icon" sizes="57x57" href="{$iconsBasePath}/apple-icon-57x57.png">
+                <link rel="apple-touch-icon" sizes="60x60" href="{$iconsBasePath}/apple-icon-60x60.png">
+                <link rel="apple-touch-icon" sizes="72x72" href="{$iconsBasePath}/apple-icon-72x72.png">
+                <link rel="apple-touch-icon" sizes="76x76" href="{$iconsBasePath}/apple-icon-76x76.png">
+                <link rel="apple-touch-icon" sizes="114x114" href="{$iconsBasePath}/Icons/apple-icon-114x114.png">
+                <link rel="apple-touch-icon" sizes="120x120" href="{$iconsBasePath}/apple-icon-120x120.png">
+                <link rel="apple-touch-icon" sizes="144x144" href="{$iconsBasePath}/apple-icon-144x144.png">
+                <link rel="apple-touch-icon" sizes="152x152" href="{$iconsBasePath}/apple-icon-152x152.png">
+                <link rel="apple-touch-icon" sizes="180x180" href="{$iconsBasePath}/apple-icon-180x180.png">
+                <link rel="icon" type="image/png" sizes="192x192"  href="{$iconsBasePath}/android-icon-192x192.png">
+                <link rel="icon" type="image/png" sizes="32x32" href="{$iconsBasePath}/favicon-32x32.png">
+                <link rel="icon" type="image/png" sizes="96x96" href="{$iconsBasePath}/favicon-96x96.png">
+                <link rel="icon" type="image/png" sizes="16x16" href="{$iconsBasePath}/favicon-16x16.png">
+                <link rel="manifest" href="{$iconsBasePath}/manifest.json">
                 <meta name="msapplication-TileColor" content="#ffffff">
-                <meta name="msapplication-TileImage" content="/Assets/Icons/ms-icon-144x144.png">
+                <meta name="msapplication-TileImage" content="{$iconsBasePath}/ms-icon-144x144.png">
                 <meta name="theme-color" content="#ffffff">
                 <title>BHN-MCPL INVOICE</title>
             </head>
