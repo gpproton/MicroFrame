63ea97e7eb3f5019d8f7514a458e9332118742f7
Godwin peter .O <me@godwin.dev>
2020-06-29 19:46:54 +0100

    Api switch

--- App/Controllers/Api/NotFoundController.php
+++ App/Controllers/Api/NotFoundController.php
@@ -9,7 +9,7 @@
  * distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so
  */
 
- namespace App\Controllers\Service;
+ namespace App\Controllers\Api;
 
  use MicroFrame\Core\Controller as Core;
 
