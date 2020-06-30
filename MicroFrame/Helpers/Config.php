605d29110d462428bb818ff3e7ecc57d83253968
Godwin peter .O <me@godwin.dev>
2020-06-30 03:08:16 +0100

    New route mode to config

--- MicroFrame/Helpers/Config.php
+++ MicroFrame/Helpers/Config.php
@@ -1,12 +1,23 @@
 <?php
 
-/* 
- * MIT License
- * Copyright 2020 - Godwin peter .O (me@godwin.dev)
- * Tolaram Group Nigeria
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files
- * (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish
- * distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so
+/**
+ * Strings helper class
+ * 
+ * PHP Version 5
+ * 
+ * @category  Helpers
+ * @package   MicroFrame
+ * @author    Godwin peter .O <me@godwin.dev>
+ * @author    Tolaram Group Nigeria <teamerp@tolaram.com>
+ * @copyright 2020 Tolaram Group Nigeria
+ * @license   MIT License
+ * @link      https://github.com/gpproton/bhn_mcpl_invoicepdf
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to 
+ * use, copy, modify, merge, publish distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so
  */
 
 namespace MicroFrame\Helpers;
@@ -16,6 +27,7 @@ final class Config {
     // Query string parameters key
     public static $BASE_REAL_PATH;
     public static $PRODUCTION_MODE;
+    public static $ROUTE_MODE;
     public static $SITE_TITLE;
     public static $STORAGE_PATH;
     public static $CACHE_PATH;
@@ -45,6 +57,7 @@ final class Config {
         ///// Variable configurations
         //
         self::$PRODUCTION_MODE = getenv('PRODUCTION_MODE') === 'true';
+        self::$ROUTE_MODE = getenv('ROUTE_MODE');
         self::$SITE_TITLE = getenv('SITE_TITLE');
         self::$STORAGE_PATH = getenv('STORAGE_PATH');
         self::$CACHE_PATH = getenv('CACHE_PATH');
@@ -53,7 +66,7 @@ final class Config {
         self::$AUTH_TIMEOUT = getenv('AUTH_TIMEOUT');
         self::$SESSION_KEY = getenv('SESSION_KEY');
 
-        self::$DATA_SOURCE = json_decode(getenv('DATA_SOURCE'), FALSE);
+        self::$DATA_SOURCE = json_decode(getenv('DATA_SOURCE'), false);
 
         self::$DATABASE_TYPE = getenv('DATABASE_TYPE');
         self::$DATABASE_HOST = getenv('DATABASE_HOST');
