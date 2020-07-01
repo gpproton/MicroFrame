5ba4f5ddd277fa08cbe62e059f7ebedc20c3478f
Godwin peter .O <me@godwin.dev>
2020-07-01 04:18:17 +0100

    Update Core.php

--- MicroFrame/Core.php
+++ MicroFrame/Core.php
@@ -1,12 +1,23 @@
 <?php
 
-/* 
- * MIT License
- * Copyright 2020 - Godwin peter .O (me@godwin.dev)
- * Tolaram Group Nigeria
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files
- * (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish
- * distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so
+/**
+ * Routes class class
+ *
+ * PHP Version 5
+ *
+ * @category  Handlers
+ * @package   MicroFrame
+ * @author    Godwin peter .O <me@godwin.dev>
+ * @author    Tolaram Group Nigeria <teamerp@tolaram.com>
+ * @copyright 2020 Tolaram Group Nigeria
+ * @license   MIT License
+ * @link      https://github.com/gpproton/bhn_mcpl_invoicepdf
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so
  */
 
 namespace MicroFrame;
@@ -24,7 +35,7 @@ final class Core {
             $nameSpacePath = str_replace('\\', '/', $class) . '.php';
             if(is_file($nameSpacePath))
             {
-                require $nameSpacePath;
+                require_once $nameSpacePath;
             }
         });
     }
