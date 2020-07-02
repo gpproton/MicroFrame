996dbda7acd71db407670cf3c2b9a987501703d0
Godwin peter .O <me@godwin.dev>
2020-07-03 00:18:12 +0100

    New core defaults

---
+++ MicroFrame/Defaults/Controller/ErrorController.php
@@ -0,0 +1,30 @@
+<?php
+defined('BASE_PATH') OR exit('No direct script access allowed');
+/**
+ * Strings helper class
+ *
+ * PHP Version 5
+ *
+ * @category  DefaultController
+ * @package   MicroFrame
+ * @author    Godwin peter .O <me@godwin.dev>
+ * @author    Tolaram Group Nigeria <teamerp@tolaram.com>
+ * @copyright 2020 Tolaram Group Nigeria
+ * @license   MIT License
+ * @link      https://github.com/gpproton/bhn_mcpl_invoicepdf
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so
+ */
+
+namespace Microframe\Defaults\Controller;
+
+use \Microframe\Core\Controller;
+
+final class ErrorController extends Controller
+{
+
+}
