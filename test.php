2b2e74a89d09eeb646aec5345fcd56b4e83f29e3
Godwin peter .O <me@godwin.dev>
2020-07-02 14:18:25 +0100

    Update test.php

--- test.php
+++ test.php
@@ -1,18 +1,28 @@
 <?php
-
-/* 
- * MIT License
- * Copyright 2020 - Godwin peter .O (me@godwin.dev)
- * Tolaram Group Nigeria
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files
- * (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish
- * distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so
+defined('BASE_PATH') OR exit('No direct script access allowed');
+/**
+ * Default options class
+ *
+ * PHP Version 5
+ *
+ * @category  App
+ * @package   MicroFrame
+ * @author    Godwin peter .O <me@godwin.dev>
+ * @author    Tolaram Group Nigeria <teamerp@tolaram.com>
+ * @copyright 2020 Tolaram Group Nigeria
+ * @license   MIT License
+ * @link      https://github.com/gpproton/bhn_mcpl_invoicepdf
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so
  */
-
 
 require_once 'vendor/autoload.php';
 
-class Test {
+class Tests {
 
     public static function Run()
     {
@@ -20,5 +30,5 @@ class Test {
     }
 }
 
-Test::Run();
+Tests::Run();
 
