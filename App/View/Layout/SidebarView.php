d18049ef7f575d125fb0e9c68352f7b0f0e3e8d8
Godwin peter .O <me@godwin.dev>
2020-07-04 20:11:42 +0100

    Restructure view layouts

---
+++ App/View/Layout/SidebarView.php
@@ -0,0 +1,32 @@
+<?php
+defined('BASE_PATH') OR exit('No direct script access allowed');
+/**
+ * Layout Sidebar view class
+ *
+ * PHP Version 5
+ *
+ * @category  View/Layout
+ * @package   App
+ * @author    Godwin peter .O <me@godwin.dev>
+ * @author    Tolaram Group Nigeria <teamerp@tolaram.com>
+ * @copyright 2020 Tolaram Group Nigeria
+ * @license   MIT License
+ * @link      https://github.com/gpproton/bhn_mcpl_invoicepdf
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so
+ */
+
+namespace App\View\Layout;
+
+final class SidebarView {
+
+    public function __construct()
+    {
+
+    }
+
+}
