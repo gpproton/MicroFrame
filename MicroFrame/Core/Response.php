b0208e8f7e867264a8cb2f0131d9e79d1cad93f2
Godwin peter .O <me@godwin.dev>
2020-07-04 20:16:05 +0100

    Add sample stream codes

--- MicroFrame/Core/Response.php
+++ MicroFrame/Core/Response.php
@@ -239,6 +239,34 @@ final class Response implements IResponse
     Public function download($path)
     {
         // TODO: Implement download() method.
+
+        //     if(Utils::urlIllegalCheckr(Path::$filePath)){
+        //         $filepath = Path::$filePath;
+        //         $filename = basename($filepath);
+        //         $filesize = filesize($filepath);
+
+        //         // Process download
+        //         if(file_exists($filepath)) {
+        //             header('Content-Description: File Transfer');
+        //             header('Content-Type: ' . Utils::mimeType($filename));
+        //             header('Content-Disposition: attachment; filename="'. $filename .'"');
+        //             header('Content-Transfer-Encoding: binary');
+        //             header('Expires: 0');
+        //             header('Cache-Control: must-revalidate');
+        //             header('Pragma: public');
+        //             header('Content-Length: ' . $filesize);
+
+        //             ob_clean();
+        //             flush();
+        //             readfile($filepath);
+        //             die();
+        //         } else {
+        //             http_response_code(404);
+        //             die();
+        //         }
+        //     } else {
+        //         die("Invalid file name!");
+        //     }
     }
 
 }
