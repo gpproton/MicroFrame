773a21041fe9f25609808d66b0388b886ed8db60
Godwin peter .O <me@godwin.dev>
2020-07-05 17:04:50 +0100

    Start logger implementation

--- MicroFrame/Handlers/Logger.php
+++ MicroFrame/Handlers/Logger.php
@@ -29,4 +29,86 @@ final class Logger {
 
     }
 
+    public static function info($output = null)
+    {
+        self::output("info", $output);
+    }
+
+    public static function error($output = null)
+    {
+        self::output("error", $output);
+    }
+
+    public static function warn($output = null)
+    {
+        self::output("warn", $output);
+    }
+
+    public static function debug($output = null)
+    {
+        self::output("debug", $output);
+    }
+
+    private static function output($type, $output)
+    {
+        switch (gettype($output)) {
+            case 'array':
+                $output =  json_encode($output);
+                break;
+            case 'null':
+                $output =  "Nothing to output here!!";
+                break;
+            case 'integer':
+                $output =  "{$output}";
+                break;
+            default:
+                $output .= $output;
+                break;
+        }
+
+        switch ($type) {
+            case 'info':
+                $type = "Info";
+                break;
+            case 'error':
+                $type = "Error";
+                break;
+            case 'warn':
+                $type = "Warn";
+                break;
+            case 'debug':
+                $type = "Debug";
+                break;
+            default:
+                $type = "Unknown";
+                break;
+        }
+
+        $output = date("Y-m-d h:i:sa") . " {$type}: " . $output;
+
+        // TODO: Plan output methods
+
+    }
+
+    private static function console() {
+
+    }
+
+    private static function web() {
+
+    }
+
+    private static function file() {
+
+    }
+
+    /**
+     * TODO: Future use case
+     */
+    private static function email() {
+
+    }
+
+
+
 }
