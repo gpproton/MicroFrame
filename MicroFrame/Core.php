7fb0005d91fc6cd0c8cb8a6c017ee97b9659090b
Godwin peter .O <me@godwin.dev>
2020-07-06 18:29:14 +0100

    Refactor core

--- MicroFrame/Core.php
+++ MicroFrame/Core.php
@@ -1,5 +1,5 @@
 <?php
-defined('BASE_PATH') OR exit('No direct script access allowed');
+
 /**
  * Core class
  *
@@ -21,6 +21,21 @@ defined('BASE_PATH') OR exit('No direct script access allowed');
  */
 
 namespace MicroFrame;
+
+define('BASE_PATH', realpath(__DIR__.'/..'));
+
+/**
+ * Set default PHP configurations.
+ */
+ini_set("short_open_tag", 1);
+ini_set("default_socket_timeout", 900);
+ini_set("max_execution_time", 900);
+ini_set("max_input_time", 600);
+ini_set("max_input_vars", 250);
+ini_set("memory_limit", -1);
+ini_set("post_max_size", "256M");
+ini_set("upload_max_filesize", "256M");
+ini_set("max_file_uploads", 300);
 
 use MicroFrame\Core\Request as request;
 use MicroFrame\Helpers\Config as config;
