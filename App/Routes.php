cc4070dfe1e56e1d28f576f4a5df957eed4512ba
Godwin peter .O <me@godwin.dev>
2020-07-15 12:42:34 +0100

    Update Routes.php

--- App/Routes.php
+++ App/Routes.php
@@ -1,31 +1,10 @@
 <?php
 
-/**
- * Default options class
- *
- * PHP Version 7
- *
- * @category  App
- * @package   App
- * @author    Godwin peter .O <me@godwin.dev>
- * @author    Tolaram Group Nigeria <teamerp@tolaram.com>
- * @copyright 2020 Tolaram Group Nigeria
- * @license   MIT License
- * @link      https://github.com/gpproton/microframe
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of
- * this software and associated documentation files (the "Software"), to deal in
- * the Software without restriction, including without limitation the rights to
- * use, copy, modify, merge, publish distribute, sublicense, and/or sell copies of
- * the Software, and to permit persons to whom the Software is furnished to do so
- */
-
 defined('BASE_PATH') OR exit('No direct script access allowed');
 
-// TODO: route point to related path with dot note, and an array of required middleware, check notes
+// TODO: Route point to related path with dot note, and an array of required middleware, check notes
 // TODO: Default app middleware config for all controllers with pattern match
 // TODO: Add default middleware, header and routing structure here.
-
 /*
 |--------------------------------------------------------------------------
 | APP Routes
