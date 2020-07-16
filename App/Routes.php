fcd380cf940f91c526895f2760314d2acc0d4187
Godwin peter .O <me@godwin.dev>
2020-07-16 19:12:43 +0100

    Route tests

--- App/Routes.php
+++ App/Routes.php
@@ -1,5 +1,7 @@
 <?php
 
+use MicroFrame\Handlers\Route;
+
 defined('BASE_PATH') OR exit('No direct script access allowed');
 
 // TODO: Route point to related path with dot note, and an array of required middleware, check notes
@@ -13,5 +15,9 @@ defined('BASE_PATH') OR exit('No direct script access allowed');
 | Here is where you can register routes for your application if they're
 | not to be automatically routed via the controller path.
 |
+| NOTE: Avoid any string or character output here it would break rendering for now.
+|
 */
+
+Route::map("/api/index/*");
 
