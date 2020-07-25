5763b666d3c3e723efd850467254a3af5e55c074
Godwin peter .O <me@godwin.dev>
2020-07-25 03:04:22 +0100

    Completed swagger default request API

--- MicroFrame/Defaults/Controller/SwaggerController.php
+++ MicroFrame/Defaults/Controller/SwaggerController.php
@@ -24,13 +24,65 @@ defined('BASE_PATH') OR exit('No direct script access allowed');
  */
 
 use \MicroFrame\Core\Controller as Core;
+use MicroFrame\Handlers\Exception;
+use MicroFrame\Library\Strings;
 use \OpenApi\Annotations as OA;
+use function OpenApi\scan;
 
 /**
  * Class SwaggerController
  * @package MicroFrame\Defaults\Controller
  */
-class SwaggerController
+class SwaggerController extends Core
 {
+    /**
+     *
+     * @OA\Info(
+     *   title="Microframe OpenAPI documetation",
+     *   version="0.0.1",
+     *   @OA\Contact(
+     *     email="teamerp@tolaram.com"
+     *   )
+     * )
+     *
+     * @OA\Get(
+     *   path="/api/swagger/*",
+     *   summary="Swagger Index API placeholder",
+     *   @OA\Response(
+     *     response=200,
+     *     description="Default base swagger API output & handling docs routing, if you're here check your code tags then ensure you're requesting for the right resource path e.g url.com/api/swagger/{api/index} or your corresponding controller base path.",
+     *     @OA\MediaType(
+     *         mediaType="application/json"
+     *     )
+     *   )
+     * )
+     *
+     */
+    public function index()
+    {
+        $apiPath = Strings::filter($this->request->path())
+            ->range(".", true, true)
+            ->replace(["swagger.api", "."], ["", "/"])
+            ->upperCaseWords()
+            ->value();
 
+        $apiPath = BASE_PATH . "/App/Controller/" . $apiPath;
+        $openApi = array();
+        try {
+            if (is_dir($apiPath)) {
+                $openApi = scan($apiPath);
+            } else {
+                throw new Exception("Directory does not exits...");
+            }
+        } catch (\Exception $exception) {
+            $openApi = scan(__DIR__);
+        } catch (Exception $exception) {
+            $exception->log($exception->message);
+        }
+
+        $this->response
+            ->methods(['get', 'post', 'put', 'delete', 'option'])
+            ->data($openApi)
+            ->send();
+    }
 }
