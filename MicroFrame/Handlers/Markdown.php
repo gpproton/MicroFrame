2a8959da01c4d328c2705aa5478aeafc7214d5d4
Godwin peter .O <me@godwin.dev>
2020-07-30 14:41:01 +0100

    Added new markdown handler

---
+++ MicroFrame/Handlers/Markdown.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Markdown Handlers class
+ *
+ * PHP Version 7
+ *
+ * @category  Handlers
+ * @package   MicroFrame\Handlers
+ * @author    Godwin peter .O <me@godwin.dev>
+ * @author    Tolaram Group Nigeria <teamerp@tolaram.com>
+ * @copyright 2020 Tolaram Group Nigeria
+ * @license   MIT License
+ * @link      https://github.com/gpproton/microframe
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so
+ */
+
+namespace MicroFrame\Handlers;
+
+defined('BASE_PATH') OR exit('No direct script access allowed');
+
+use cebe\markdown\MarkdownExtra;
+
+/**
+ * Class Markdown
+ * @package MicroFrame\Handlers
+ */
+class Markdown extends MarkdownExtra
+{
+
+}
