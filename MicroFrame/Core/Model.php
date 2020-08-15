3b6e8cfdb3f43115f5c198145a058a3383b0aced
Godwin peter .O <me@godwin.dev>
2020-08-15 16:10:32 +0100

    New annotation format

--- MicroFrame/Core/Model.php
+++ MicroFrame/Core/Model.php
@@ -1,5 +1,22 @@
 <?php
-
+/**
+ * Core Model class
+ *
+ * PHP Version 7
+ *
+ * @category  Core
+ * @package   MicroFrame\Core
+ * @author    Tolaram Group Nigeria <teamerp@tolaram.com>
+ * @copyright 2020 Tolaram Group Nigeria
+ * @license   MIT License
+ * @link      https://github.com/gpproton/microframe
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so
+ */
 namespace MicroFrame\Core;
 
 defined('BASE_PATH') or exit('No direct script access allowed');
@@ -14,21 +31,11 @@ use PDO;
 /**
  * Model class
  *
- * PHP Version 7
- *
- * @category  Core
- * @package   MicroFrame\Core
- * @author    Godwin peter .O <me@godwin.dev>
- * @author    Tolaram Group Nigeria <teamerp@tolaram.com>
- * @copyright 2020 Tolaram Group Nigeria
- * @license   MIT License
- * @link      https://github.com/gpproton/microframe
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of
- * this software and associated documentation files (the "Software"), to deal in
- * the Software without restriction, including without limitation the rights to
- * use, copy, modify, merge, publish distribute, sublicense, and/or sell copies of
- * the Software, and to permit persons to whom the Software is furnished to do so
+ * @category Core
+ * @package  MicroFrame\Core
+ * @author   Godwin peter .O <me@godwin.dev>
+ * @license  MIT License
+ * @link     https://github.com/gpproton/microframe
  */
 final class Model implements IModel
 {
