c015ae92623ac13af8bc6ce3f6122f47ab2a90b6
Godwin peter .O <me@godwin.dev>
2020-08-15 17:08:13 +0100

    Improve HTML metadatas

--- MicroFrame/Defaults/View/DefaultView.php
+++ MicroFrame/Defaults/View/DefaultView.php
@@ -1,10 +1,33 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel=“canonical” href=“<?=$url?>” />
     <title><?=$errorTitle?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!--Import Google Icon Font-->
     <link href="<?=$root?>css/main.css" rel="stylesheet">
+    <!-- Load CSS lib -->
     <link rel="stylesheet" href="<?=$root?>css/materialize.min.css" media="screen,projection">
+    <!-- Link required icons -->
+    <link rel="apple-touch-icon" sizes="57x57" href="<?=$root?>icons/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="<?=$root?>icons/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="<?=$root?>icons/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="<?=$root?>icons/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="<?=$root?>icons/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="<?=$root?>icons/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="<?=$root?>icons/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="<?=$root?>icons/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?=$root?>icons/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="<?=$root?>icons/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?=$root?>icons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="<?=$root?>icons/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?=$root?>icons/favicon-16x16.png">
+    <link rel="manifest" href="<?=$root?>icons/manifest.json">
+    <meta name="msapplication-TileColor" content="#ededed">
+    <meta name="msapplication-TileImage" content="<?=$root?>icons/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ededed">
     <style>
         body {
             font-weight: 300 !important;
--- MicroFrame/Defaults/View/SwaggerUIView.php
+++ MicroFrame/Defaults/View/SwaggerUIView.php
@@ -3,6 +3,8 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel=“canonical” href=“<?=$url?>” />
     <title>MicroFrame API</title>
     <link rel="stylesheet" type="text/css" href="<?=$root?>swagger-ui.css" >
     <link rel="icon" type="image/png" href="<?=$root?>favicon-32x32.png" sizes="32x32" />
