b2fcbda0bfaa073e0cce066400b006396e44ae57
Godwin peter .O <me@godwin.dev>
2020-09-01 16:20:12 +0100

    New Library samples

---
+++ App/Library/BaseTest.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * BaseTest class
+ *
+ * PHP Version 7
+ *
+ * @category  MicroFrame
+ * @package   App\Library
+ * @author    Godwin peter .O <me@godwin.dev>
+ * @author    Tolaram Group Nigeria <teamerp@tolaram.com>
+ * @copyright 2020 Tolaram Group Nigeria
+ * @license   MIT License
+ * @link      https://github.com/gpproton/microframe
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so
+ */
+
+namespace App\Library;
+
+/**
+ * Class Test
+ *
+ * @category Library
+ * @package  App\Library
+ * @author   Godwin peter .O <me@godwin.dev>
+ * @license  MIT License
+ * @link     https://godwin.dev
+ */
+class BaseTest
+{
+
+}
